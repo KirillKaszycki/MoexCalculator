5c71f7069392eabd76330769eded528bbed72e95
KirillKaszycki <kashitskiikirill@gmail.com>
2024-08-04 12:30:57 +0500

    Initial Commit

---
+++ MoexCalculator/MoexCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexCalculatorApp.swift
+//  MoexCalculator
+//
+//  Created by Кирилл Кашицкий on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
